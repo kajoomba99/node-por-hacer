6cf45232296c40b74f8e2086b0c802b46edf93b2
Steven <steven.brand1999@gmail.com>
2020-01-08 21:17:20 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const { argv } = require("./config/yargs");
+const {crear, getListado, actualizar, borrar} = require("./por-hacer/por-hacer")
+const colors = require('colors')
+let comando = argv._[0];
+
+switch (comando) {
+  case "crear":
+    let tarea = crear(argv.descripcion)
+    console.log(tarea);
+    break;
+  case "listar":
+    let listado = getListado();
+
+    for(let tarea of listado){
+        console.log("=====Por Hacer=====".green);
+        console.log(tarea.descripcion);
+        console.log("Estaado:",tarea.completado);
+        console.log("===================".green);
+    }
+
+    break;
+  case "actualizar":
+    let actualizado = actualizar(argv.descripcion, argv.completado)
+    console.log(actualizado);
+    break;
+  case "borrar":
+      let borrado = borrar(argv.descripcion)
+      console.log(borrado);
+      break;
+  default:
+    console.log("Comando no reconocido");
+    break;
+}
